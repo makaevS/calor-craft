3756d65aabd621783000636832d80014b368e067
Stepan Makaev <makaev.stepan@vk.com>
2026-03-01 18:41:42 +0500

    wip

--- apps/api/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-# Собираем DATABASE_URL из POSTGRES_*, если не задан (для контейнера)
-if [ -z "$DATABASE_URL" ]; then
-  export DATABASE_URL="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST:-postgres}:5432/${POSTGRES_DB}"
-fi
-exec "$@"
